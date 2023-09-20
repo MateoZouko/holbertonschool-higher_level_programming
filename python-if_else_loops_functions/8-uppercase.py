eb37c429a6900f8c29203a25bc31be8746cb94d9
Mateo Zouko <mateozouko1@gmail.com>
2023-09-20 17:48:50 +0000

    fixing task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def uppercase(input_str):
-    for char in input_str:
-        char_code = ord(char)
-        if 97 <= char_code <= 122:
-            uppercase_char = chr(char_code - 32)
-        else:
-            uppercase_char = char
-        print(uppercase_char, end="")
-
-    print()
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
