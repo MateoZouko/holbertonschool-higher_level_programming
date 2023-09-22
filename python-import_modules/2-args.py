6400d40497db16cc2970efdc80117e54419a74fb
Mateo Zouko <mateozouko1@gmail.com>
2023-09-22 15:52:04 +0000

    fixing task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,8 @@ import sys
 if (len(sys.argv)) == 1:
     print("0 arguments.")
 elif (len(sys.argv)) == 2:
-    print("{} argument:".format(len(sys.argv)))
+    print("{} argument:".format(len(sys.argv)-1))
+    print("{}: {}".format((len(sys.argv)) - 1, sys.argv[1]))
 else:
-    for n in range (1, sys.argv):
+    for n in range (1, len(sys.argv)):
         print("{}: {}".format(n, sys.argv[n]))
