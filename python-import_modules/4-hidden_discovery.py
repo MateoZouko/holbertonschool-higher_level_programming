a0ad32e99d77372a5896be4cb11f4fff5467a6fc
Mateo Zouko <mateozouko1@gmail.com>
2023-09-22 18:36:15 +0000

    task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    
-    names = dir(hidden_4)
-    for name in names:
+
+    list = dir(hidden_4)
+    for name in list:
         if name[:2] != "__":
             print(name)
