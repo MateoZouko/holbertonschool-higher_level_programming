2ddb18eb61762940bcbe168da9cbf1aa1e3cbdfb
Mateo Zouko <mateozouko1@gmail.com>
2023-09-22 16:06:18 +0000

    fixing task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if (len(sys.argv)) <= 1:
-    print("{} arguments.".format(len(sys.argv) - 1))
-elif (len(sys.argv)) == 2:
-    print("{} argument:".format("1"))
-    print("1: {}".format( sys.argv[1]))
-if (len(sys.argv) > 2):
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for n in range (1, len(sys.argv)):
-        print("{}: {}".format(n, sys.argv[n]))
+    if (len(sys.argv)) <= 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif (len(sys.argv)) == 2:
+        print("{} argument:".format("1"))
+        print("1: {}".format( sys.argv[1]))
+    if (len(sys.argv) > 2):
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for n in range (1, len(sys.argv)):
+            print("{}: {}".format(n, sys.argv[n]))
