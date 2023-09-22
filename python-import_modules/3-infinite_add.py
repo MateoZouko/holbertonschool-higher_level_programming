567bd9dcca18d64f9a41d4c0889c09b5df1c3c4a
Mateo Zouko <mateozouko1@gmail.com>
2023-09-22 18:05:00 +0000

    fixing pycodestyle

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     suma = 0
-    for n in range (1, len(argv)):
+    for n in range(1, len(argv)):
         suma = suma + int(argv[n])
-    
+
 print(suma)
