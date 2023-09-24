7d67ef9f23bca160f78fa6fbca50be64023aa885
Mateo Zouko <mateozouko1@gmail.com>
2023-09-24 15:11:39 +0000

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
+    if len(tuple_a) == 0:
+        new_tuple = (0, 0)
+    elif len(tuple_a) < 2:
         new_tuple = (tuple_a[0], 0)
     else:
         new_tuple = tuple_a[:2]
