1c4aabebad267656f76fbd4dd894b808ace97de5
Mateo Zouko <mateozouko1@gmail.com>
2023-09-24 15:21:52 +0000

    fixing task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        length = len(sentence)
-        letter = sentence[0]
-        return(length, letter)
-    return None
+    if (sentence):
+        lens = len(sentence)
+        a = sentence[0]
+        return (lens, a)
+    return(None)
