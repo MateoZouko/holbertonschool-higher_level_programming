ac9f9d6b17b41af4a34f9cdaeff5d89fd6a099cc
Mateo Zouko <mateozouko1@gmail.com>
2023-09-24 13:32:44 +0000

    fixing task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for i, element in enumerate(line):
             if i == len(line) - 1:
-                print("{}".format(element), end='')
+                print("{:d}".format(element), end='')
             else:
-                print("{} ".format(element), end='')
+                print("{:d}".format(element), end=' ')
         print()
