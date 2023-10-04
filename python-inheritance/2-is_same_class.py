0c336fcac128ecef47050e6386a9f53003cf1941
Mateo Zouko <mateozouko1@gmail.com>
2023-10-04 14:04:59 +0000

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function is same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    return true if type(obj) is a class
+    if not return False
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
