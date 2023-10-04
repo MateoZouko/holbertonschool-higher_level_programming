feb382fdb9ea167f4aa3753ccc90e556636f920c
Mateo Zouko <mateozouko1@gmail.com>
2023-10-04 17:08:43 +0000

    fixing task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+Summary
+"""
+
+
 def inherits_from(obj, a_class):
-    return issubclass(type(obj), a_class)
+    """
+    defining inherits from
+    """
+    if isinstance(obj, a_class) and not type(obj) is a_class:
+        return True
+    else:
+        return False
