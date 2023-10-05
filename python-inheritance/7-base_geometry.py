0fa6107d331e9a6bdbd947334feafdd702345c1e
Mateo Zouko <mateozouko1@gmail.com>
2023-10-05 13:09:12 +0000

    task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,7 @@ class BaseGeometry:
         defining area
         """
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """
         defining integrer validator
