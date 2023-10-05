d3e1dfb8cdeca131ffe954670e0d176633207340
Mateo Zouko <mateozouko1@gmail.com>
2023-10-05 12:53:51 +0000

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Summary
+"""
+
+
+class BaseGeometry:
+    """
+    Clase BaseGeometry definida
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
