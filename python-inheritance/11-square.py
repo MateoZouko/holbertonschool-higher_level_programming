0e6906695dad8e75e2be1e73e7387eb64b89853e
Mateo Zouko <mateozouko1@gmail.com>
2023-10-06 13:57:41 +0000

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This module contains the class 'Square'
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    setting class
+    """
+    def __init__(self, size):
+        """
+        defining init
+        """
+        super().__init__(size, size)
