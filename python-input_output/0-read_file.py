052d06bf948f80b8622decf72d429c9a89484ff6
Mateo Zouko <mateozouko1@gmail.com>
2023-10-06 14:26:53 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,13 +2,12 @@
 """
 Write a function that reads a text file (UTF8) and prints it to stdout:
 """
+
+
 def read_file(filename=""):
     """
     defining read file function
     """
-    try:
-        with open(filename, encoding="utf-8") as file:
-            for line in file:
-                print(line, end="")
-    except FileNotFoundError:
-        pass
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
