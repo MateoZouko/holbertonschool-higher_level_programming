3b368df1c40d80fa01b8bb4e213cf4e04e4548d7
Mateo Zouko <mateozouko1@gmail.com>
2023-10-06 14:56:34 +0000

    fixing task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-Write a function that reads a text file (UTF8) and prints it to stdout:
-"""
+"""Write a function that reads a text file (UTF8) and prints it to stdout:"""
 
 
 def read_file(filename=""):
-    """
-    defining read file function
-    """
+    """defining read file function"""
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read(), end="")
