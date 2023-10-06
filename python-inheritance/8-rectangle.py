b1cf1131dc875e4c9ade91ca3c736f7ee9c668a8
Mateo Zouko <mateozouko1@gmail.com>
2023-10-06 13:31:08 +0000

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,7 +6,7 @@ Task 8
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseGeometry):  
+class Rectangle(BaseGeometry):
     """
     Defining Rectangle class
     """
