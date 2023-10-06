2ffd5f433a6e31bcf3a329c13c93824aef9f46c9
Mateo Zouko <mateozouko1@gmail.com>
2023-10-06 13:30:04 +0000

    fixing task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,34 +3,14 @@
 """
 Task 8
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """
-    Clase BaseGeometry definida
-    """
-    def area(self):
-        """
-        defining area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        defining integrer validator
-        """
-        if isinstance(name, str):
-            if type(value) is not int:
-                raise TypeError(f"{name} must be an integer")
-            if value <= 0:
-                raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
+class Rectangle(BaseGeometry):  
     """
     Defining Rectangle class
     """
-    def __init__(self, width=0, height=0):
+    def __init__(self, width, height):
         """
         initializing
         """
