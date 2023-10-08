d8a21bea9c8083e31ce49ee30e97a6f04ccb3292
Mateo Zouko <mateozouko1@gmail.com>
2023-10-08 16:18:53 +0000

    feat

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 """
-    Defining a class called Student
+    Module defining a class called Student
 """
 
 
 class Student:
     """
     Defining Student class with fist_name,
-    last_age and age
+    last_name and age
     """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
-        self.last_age = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
