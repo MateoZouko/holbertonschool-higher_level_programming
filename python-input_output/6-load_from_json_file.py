ae345146d5d54a0d62e1b12ab1d5b03b17754dfc
Mateo Zouko <mateozouko1@gmail.com>
2023-10-08 14:03:45 +0000

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Write a function that creates
+an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """Defining new function"""
+    with open(filename, 'r') as file:
+        data = json.load(file)
