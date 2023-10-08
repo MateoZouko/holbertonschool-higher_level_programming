fe677fcef24821b2d10ba40a0d952e13d895d9fc
Mateo Zouko <mateozouko1@gmail.com>
2023-10-08 16:11:53 +0000

    adding comment

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,6 +5,10 @@
 
 
 class Stundent:
+    """
+    Defining Student class with fist_name,
+    last_age and age
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_age = last_name
