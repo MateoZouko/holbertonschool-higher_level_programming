40afca2f3523b5f62519ed0593657c2951611f7b
Mateo Zouko <mateozouko1@gmail.com>
2023-10-14 17:45:53 +0000

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
     Task 1: create a class called Base
 """
+import json
 
 
 class Base:
@@ -19,3 +20,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod  
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        if len(list_dictionaries) <= 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
