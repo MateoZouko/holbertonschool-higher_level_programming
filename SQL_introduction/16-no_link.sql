cb1639c868c128a3fdd58f4d25777f39788fd8a0
Mateo Zouko <mateozouko1@gmail.com>
2023-11-15 17:07:50 +0000

    feat

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,8 +1,6 @@
 -- script that lists all records of the table
 
--- script that lists the number of records with the same score in the table
-
-SELECT score name
+SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
