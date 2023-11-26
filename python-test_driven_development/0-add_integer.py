e9e2bbcb2181e00c3672434858492ec5272b94da
Mateo Zouko <mateozouko1@gmail.com>
2023-11-26 00:24:49 +0000

    feat

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,8 +12,9 @@ integer or b must be an integer
 a and b must be first casted to integers if they are float
 Returns an integer: the addition of a and b
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    if not (isinstance(a, int) or isinstance(a, float)) or \
+       not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
